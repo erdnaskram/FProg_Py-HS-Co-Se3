77dd76ce0d665b91d2e29dd152827a3cae294d8f
erdnaskram <erdnaskram@users.noreply.github.com>
2021-10-24 23:46:14 +0200

    kap2 Aufagbe 10-12

--- src/Python/kap2/Aufgabe_11.py
+++ src/Python/kap2/Aufgabe_11.py
@@ -1,20 +1,20 @@
-damen = ("d1","d2","d3","d4",)
-herren = ("h1","h2","h3","h4",)
+damen = ("d1", "d2", "d3", "d4",)
+herren = ("h1", "h2", "h3", "h4",)
 trainer = ("t1",)
 
 anzhalTanzpaare = 0
 
 for i in range(len(damen)):
     for j in range(len(herren)):
-        print(damen[i]," mit ",herren[j])
+        print(damen[i], " mit ", herren[j])
         anzhalTanzpaare += 1
     for j in range(len(trainer)):
-        print(damen[i]," mit ",trainer[j])
+        print(damen[i], " mit ", trainer[j])
         anzhalTanzpaare += 1
 
 for i in range(len(herren)):
     for j in range(len(trainer)):
-        print(herren[i]," mit ",trainer[j])
+        print(herren[i], " mit ", trainer[j])
         anzhalTanzpaare += 1
 
 print("\nAnzahl der Paare: ", anzhalTanzpaare)
